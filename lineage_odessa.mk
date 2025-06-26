78eb101778e8b873f5fc529c73d128a2757fb8e8
VotikaBr <rafael1.spindola@gmail.com>
2025-06-26 20:54:35 -0300

    Commit inicial para o dispositivo Odessa
    Change-Id: I88b32efb846c8d7e8298288a2c5f51187b50ae73

--- lineage_odessa.mk
+++ lineage_odessa.mk
@@ -21,34 +21,41 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from odessa device
 $(call inherit-product, device/motorola/odessa/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Flags
+# Feature flags
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_DISABLE_EPPE := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# aospa gms
-#$(call inherit-product, vendor/google/gms/gms-vendor.mk)
-
-# Signing Build
--include vendor/lineage-priv/keys/keys.mk
-
-# Device identifier. This must come after all inclusions.
+# Device identifiers
 PRODUCT_NAME := lineage_odessa
 PRODUCT_DEVICE := odessa
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(9) plus
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_AAPT_CONFIG := xxxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
-
-# Build info
-BUILD_FINGERPRINT := "motorola/odessa_retail/odessa:11/RPAS31.Q2-59-17-4-5-5/af8e3:user/release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=odessa_retail \
-    PRIVATE_BUILD_DESC="odessa_retail-user 11 RPAS31.Q2-59-17-4-5-5 af8e3 release-keys"  
-
+# GMS Client ID
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Display density
+# The build system automatically selects the best density.
+# Specifying it here is often redundant unless you need a specific override.
+# PRODUCT_AAPT_CONFIG := xxxhdpi
+# PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# ===================================================================
+# CORREÇÃO PRINCIPAL AQUI
+# ===================================================================
+# Define build fingerprint and description as direct Make variables.
+# This avoids the "isn't a valid prop override" error.
+# This should match a stock firmware build for Play Integrity.
+
+BUILD_FINGERPRINT := "motorola/odessa_retail/odessa:11/RPAS31.Q2-59-17-4-5-5/af8e3:user/release-keys"
+PRIVATE_BUILD_DESC := "odessa_retail-user 11 RPAS31.Q2-59-17-4-5-5 af8e3 release-keys"
+
+# We can use PRODUCT_PROPERTY_OVERRIDES to set the final property if needed,
+# but setting PRIVATE_BUILD_DESC is the cleaner way to set ro.build.description.
+# The build system uses PRIVATE_BUILD_DESC to generate ro.build.description.
+# Example of the less clean way (for reference only
